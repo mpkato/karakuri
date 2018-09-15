d3efa5d0323c7582cf8df75b91a6d8954a0352ac
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2018-09-15 21:35:37 +0900

    Upgrade ruby

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :pty, true
 set :keep_releases, 5
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.4'
+set :rbenv_ruby, '2.4.3'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
