91eaeea7d5fa3816d9ecbd84b791d2c6fcf67bb9
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2018-09-15 22:07:55 +0900

    Fix a bug of deplicated method 'for' in devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |u|
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
       u.permit(:username, :email, :password, :password_confirmation)
     end
-    devise_parameter_sanitizer.for(:account_update) do |u|
+    devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:username, :email, :password, :password_confirmation, :current_password)
     end
   end
